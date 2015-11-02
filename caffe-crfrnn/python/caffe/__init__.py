c640757a00049904a8e80302f3539492e538a5c3
Xiaohui Zeng <xizeng@icivrgsrv1.epfl.ch>
2015-11-02 01:05:47 +0100

    add nesseary file

--- caffe-crfrnn/python/caffe/__init__.py
+++ caffe-crfrnn/python/caffe/__init__.py
@@ -2,4 +2,6 @@ from .pycaffe import Net, SGDSolver
 from .classifier import Classifier
 from .detector import Detector
 from .segmenter import Segmenter
+from .read_net import Read_net
+from .get_net import Get_net
 import io
---
+++ caffe-crfrnn/python/caffe/get_net.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+
+# # Instant Return of features of differnet layer with Caffe
+# 
+# input an image with the bundled CaffeNet model based on the pretrained model 
+# collect the output 'features' of defferent layer and save as .mat file
+# First, import required modules, 
+# set plotting parameters, and 
+# run `./scripts/download_model_binary.py 
+# models/bvlc_reference_caffenet` 
+# to get the pretrained CaffeNet model if it hasn't already been fetched.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Make sure that caffe is on the python path:
+caffe_root = '../'  # this file is expected to be in caffe/python/caffe
+import sys
+
+from subprocess import call 
+import scipy.io as sio
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+import os
+# image_name = 'cat'
+def Get_net(MODEL,image_name):
+	image_input = caffe_root + 'examples/images/' + image_name + '.jpg'
+#	MODEL = 'bvlc_reference_caffenet'
+
+        #'../../models/bvlc_reference_caffenet'
+	MODELFILE_DOWNLOAD = caffe_root+ 'models/'+ MODEL
+	MODELPATH = MODELFILE_DOWNLOAD + '/'
+	MODELFILE = MODELPATH + MODEL +'.caffemodel'
+	deploy_proto = MODELPATH + 'deploy.prototxt'
+	print("finding: ",MODELFILE)
+	if not os.path.isfile(MODELFILE):
+	    print("Call and Downloading pre-trained CaffeNet model",MODELFILE_DOWNLOAD)
+	    call([caffe_root + 'scripts/download_model_binary.py',MODELFILE_DOWNLOAD])
+
+	# Set Caffe to CPU mode, 
+	# load the net in the test phase for inference, 
+	# and configure input preprocessing.
+
+	caffe.set_mode_cpu()
+	net = caffe.Net(deploy_proto,
+	                MODELFILE,
+	                caffe.TEST)
+	mean_file = caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy'
+
+	# input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+	transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+	transformer.set_transpose('data', (2,0,1))
+	transformer.set_mean('data', np.load(mean_file).mean(1).mean(1)) 
+	# mean pixel
+
+	# the reference model operates on images in [0,255] range instead of [0,1]
+	transformer.set_raw_scale('data', 255)  
+
+	# the reference model has channels in BGR order instead of RGB
+	transformer.set_channel_swap('data', (2,1,0))  
+
+	# Let's start with a simple classification. We'll set a batch of 50 to demonstrate batch processing, even though we'll only be classifying one image. (Note that the batch size can also be changed on-the-fly.)
+
+	# set net to batch size of 50
+	net.blobs['data'].reshape(50,3,227,227)
+
+
+	# Feed in the image (with some preprocessing) and classify with a forward pass.
+	net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(image_input))
+	out = net.forward()
+	# print("Predicted class is #{}.".format(out['prob'].argmax()))
+	# print net.blobs['data'].data[0]
+
+	# CPU mode
+	net.forward()  
+	# call once for allocation
+	return net
+
+
+
+
